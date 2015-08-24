42e93a825b544d84b44d35071d69b39339e99066
Pablo Alejandro Costesich <rhfixer@gmail.com>
2015-08-24 01:26:53 -0300

    Quick'n'dirty implementation of route insert

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,64 @@
 'use strict';
 
-var generators = require('yeoman-generator');
+var generators = require('yeoman-generator')
+  , esprima = require('esprima')
+  , Promise = require('bluebird')
+  ;
+
+
+function parseAst(file) {
+  return esprima.parse(file, {
+    comment: true,
+    loc: true
+  });
+}
+
+function findRouteInsertionPoint (file) {
+  var i
+    , ast = parseAst(file)
+    , comment
+    , comments = ast.comments
+    , regex = /generator - insert routes here/gi
+    ;
+
+  for (i = 0; i < comments.length; i++) {
+    comment = comments[i];
+    if (comment.value.match(regex)) {
+      return comment.loc.start.line;
+    }
+  }
+}
+
+var prompts = {
+  askForController: {
+    type: 'confirm',
+    name: 'addRoute',
+    message: 'Do you want to add a route to this controller?',
+    default: true // Default to current folder name
+  },
+
+  askForRoute: {
+    type: 'input',
+    name: 'route',
+    message: 'Path to route?',
+    default: '/',
+    validate: function(response) {
+      return response != '';
+    }
+  },
+
+  askForMethod: {
+    type: 'list',
+    name: 'method',
+    message: 'Method?',
+    choices: ['get', 'post', 'put', 'delete'],
+    default: 'get' // Default to current folder name
+  },
+
+};
 
 module.exports = generators.Base.extend({
-  constructor: function () {
+  constructor: function() {
     generators.Base.apply(this, arguments);
     this.argument('name', {
       required: true,
@@ -12,7 +67,30 @@ module.exports = generators.Base.extend({
     });
   },
 
-  writing: function () {
+  prompting: function() {
+    var done = this.async()
+      , prompter = this
+      , extras = [prompts.askForMethod, prompts.askForRoute]
+      ;
+
+    function doneAsking(answers) {
+      prompter.route = answers.route;
+      prompter.method = answers.method;
+      return done();
+    }
+
+    this.prompt(prompts.askForController, function (answers) {
+      if (!answers.addRoute) {
+        return done();
+      }
+
+      prompter.addRoute = answers.addRoute;
+      prompter.prompt(extras, doneAsking);
+    });
+  },
+
+  writing: function() {
+    
   	var commonEnv = {
   	  controllerName: this.name
     };
@@ -23,5 +101,20 @@ module.exports = generators.Base.extend({
       commonEnv
     );
 
+    if (this.addRoute) {
+      var path = this.destinationPath('app/routes.js')
+        , file = this.fs.read(path)
+        , point = findRouteInsertionPoint(file)
+        , insertion
+        , contents = file.toString().split('\n')
+        ;
+
+      insertion = ('router.' + this.method +
+        "('" + this.route + "', controller('" + this.name + "');");
+
+      contents.splice(point, 0, insertion);
+
+      this.fs.write(path, contents.join('\n'));
+    }
   }
 });
